1ca2e0f1b10656f97be921f47c6d66361de6b4b4
Will James <mtekimaki@gmail.com>
2014-03-21 14:10:51 -0400

    move interaction properties out of twitter - already in interactions tables, move hashtags and mentions out of twitter - use interactions augmentation

--- sources/twitter/postgresql.sql
+++ sources/twitter/postgresql.sql
@@ -5,18 +5,15 @@ CREATE TABLE twitter (
   author_name text DEFAULT NULL,
   author_username text DEFAULT NULL,
   author_language VARCHAR(64) DEFAULT NULL,
-  source VARCHAR(64) DEFAULT NULL,
   interaction_link TEXT DEFAULT NULL,
   followers_count INT DEFAULT NULL,
   user_description TEXT DEFAULT NULL,
   user_timezone VARCHAR(64) DEFAULT NULL,
   user_location VARCHAR(64) DEFAULT NULL,
   twitter_place_country VARCHAR(64) DEFAULT NULL,
+  twitter_place_full_name VARCHAR(64) DEFAULT NULL,
   verified INT DEFAULT NULL,
   twitter_id VARCHAR(64) DEFAULT NULL,
-  geo_latitude DECIMAL DEFAULT NULL,
-  geo_longitude DECIMAL DEFAULT NULL,
-  content TEXT DEFAULT NULL,
   twitter_lang VARCHAR(64) DEFAULT NULL,
   is_retweet INTEGER DEFAULT NULL,
   twitter_retweeted_id VARCHAR(64) DEFAULT NULL,
@@ -25,28 +22,3 @@ CREATE TABLE twitter (
 );
 CREATE INDEX twitter_interaction_type_idx ON twitter (interaction_type);
 CREATE INDEX twitter_created_at_idx ON twitter (created_at);
-
-
-CREATE TABLE hashtags (
-  interaction_id VARCHAR(64) NOT NULL,
-  interaction_type VARCHAR(64) NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  hashtag TEXT DEFAULT NULL
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
-CREATE INDEX hashtags_interaction_id_idx ON hashtags (interaction_id);
-CREATE INDEX hashtags_interaction_type_idx ON hashtags (interaction_type);
-CREATE INDEX hashtags_created_at_idx ON hashtags (created_at);
-
-
-CREATE TABLE mentions (
-  interaction_id VARCHAR(64) NOT NULL,
-  interaction_type VARCHAR(64) NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  mention TEXT DEFAULT NULL
-);
-CREATE INDEX mentions_interaction_id_idx ON mentions (interaction_id);
-CREATE INDEX mentions_interaction_type_idx ON mentions (interaction_type);
-CREATE INDEX mentions_created_at_idx ON mentions (created_at);
