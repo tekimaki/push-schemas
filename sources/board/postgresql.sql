f2e526a9bfbbe8bbdd94a24ccfd9cb13edc35213
Will James <mtekimaki@gmail.com>
2014-03-21 13:09:25 -0400

    update psql schema with schema from mysql

--- sources/board/postgresql.sql
+++ sources/board/postgresql.sql
@@ -1,19 +1,38 @@
-CREATE TABLE IF NOT EXISTS amazon (
+CREATE TABLE IF NOT EXISTS board (
   interaction_id VARCHAR(64) PRIMARY KEY,
   created_at TIMESTAMP DEFAULT NULL,
-  author_name TEXT DEFAULT NULL,
-  author_age INTEGER DEFAULT NULL,
-  author_gender TEXT DEFAULT NULL,
-  author_link TEXT DEFAULT NULL,
-  author_location TEXT DEFAULT NULL,
-  author_url TEXT DEFAULT NULL,
+  anchor VARCHAR(255) DEFAULT NULL,
+  author_age TEXT DEFAULT NULL,
+  author_avatar VARCHAR(255) DEFAULT NULL,
+  author_gender VARCHAR(64) DEFAULT NULL,
+  author_link VARCHAR(255) DEFAULT NULL,
+  author_location VARCHAR(255) DEFAULT NULL,
+  author_name VARCHAR(255) DEFAULT NULL,
+  author_registered TEXT DEFAULT NULL,
+  author_username VARCHAR(255) DEFAULT NULL,
+  boardname VARCHAR(255) DEFAULT NULL,
+  categories VARCHAR(255) DEFAULT NULL,
   content TEXT DEFAULT NULL,
-  contenttype TEXT DEFAULT NULL,
-  domain TEXT DEFAULT NULL,
-  amazon_id TEXT DEFAULT NULL,
-  link TEXT DEFAULT NULL,
-  title TEXT DEFAULT NULL,
-  subtype TEXT DEFAULT NULL
+  contenttype VARCHAR(64) DEFAULT NULL,
+  countrycode VARCHAR(4) DEFAULT NULL,
+  crawled TEXT DEFAULT NULL,
+  domain VARCHAR(255) DEFAULT NULL,
+  forumid VARCHAR(64) DEFAULT NULL,
+  forumname VARCHAR(255) DEFAULT NULL,
+  forumurl TEXT DEFAULT NULL,
+  gmt TEXT DEFAULT NULL,
+  language VARCHAR(64) DEFAULT NULL,
+  link VARCHAR(255) DEFAULT NULL,
+  links TEXT DEFAULT NULL,
+  review_recommendation TEXT DEFAULT NULL,
+  review_ticker TEXT DEFAULT NULL,
+  siteid VARCHAR(64) DEFAULT NULL,
+  thread VARCHAR(255) DEFAULT NULL,
+  threadid INTEGER DEFAULT NULL,
+  threadstarter TEXT DEFAULT NULL,
+  title VARCHAR(255) DEFAULT NULL,
+  topics VARCHAR(255) DEFAULT NULL,
+  board_type VARCHAR(64) DEFAULT NULL
 );
-CREATE INDEX amazon_interaction_id_idx ON amazon (interaction_id);
-CREATE INDEX amazon_created_at_idx ON amazon (created_at);
+CREATE INDEX board_interaction_id_idx ON board (interaction_id);
+CREATE INDEX board_created_at_idx ON board (created_at);
